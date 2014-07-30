e96dcb5b5d6c179c7c2428620199d812923f9315
Mithrilhall <spamquagmire@gmail.com>
2014-07-30 10:25:00 -0400

    Made major changes. Should process faster with less errors. Errors are now logged to an error.log file.

--- GraphicsMagick.js
+++ GraphicsMagick.js
@@ -1,131 +1,123 @@
 var gm = require('gm');
 var im = require("imagemagick")
-var fs = require('fs');
+var fs = require('fs-extra');
 var path = require('path');
 var async = require('async');
-var cnt = 1;
+var mkdirp = require('mkdirp');
+var src = process.argv[2];
+var dst = process.argv[3];
+var now = new Date();
+var jsonDate = now.toJSON();
 
 var processQueue = async.queue(function (task, callback) {
     var basename = path.basename(task.file.toLowerCase(), '.jpg');
     var dirname = path.dirname(task.file);
+    var newfile = dirname.replace(src, dst) + '\\' + basename + task.suffix + '.jpg';
 
-    gm(task.file)
-        .size(function (err, size) {
-            if (!err) {
-                if (size.width > task.width) {
-                    gm(task.file)
-                        .resize(task.width)
-                        .write(dirname + '/' + basename.split('.')[0] + task.suffix + '.jpg', function (err) {
-                            if (err) {
-                                console.log('Uh Oh: ' + err);
-                                return console.dir(arguments);
-                            }
-                            console.log('-----------------------------------------------\n');
-                            console.log(this.outname + " created " + arguments[3]);
-                            console.log('-----------------------------------------------\n\n');
-                        });
-                }
+    fs.ensureDir(dirname.replace(src, dst), function (err) {
+        if (!err) {
+            gm(dirname + '\\' + basename + '.jpg')
+                .resize(task.width)
+                .write(newfile, function (err) {
+                    if (err) {
+                        console.log('Uh Oh: ' + err);
+                        fs.appendFileSync('error.log', jsonDate + '\t\t' + err);
+                        return console.dir(arguments);
+                    }
+                    console.log('-----------------------------------------------\n');
+                    console.log("Created " + arguments[3]);
+                    console.log('-----------------------------------------------\n\n');
+                });
+            callback(null);
+        }
+    })
+}, 10);
+
+
+var walk = function (root) {
+
+    if (src && dst) {
+        var files = fs.readdirSync(root);
+
+        // Make directories
+        for (var x in files) {
+            var file = path.join(root, files[x]);
+            var stat = fs.statSync(file);
+
+
+            if (stat.isDirectory()) {
+                // Create Directories
+//                mkdirp(file.replace(src, dst), function (err) {
+//                    if (err) {
+//                        console.log(err);
+//                        fs.appendFileSync('error.log', err);
+//                    }
+//                });
+                fs.ensureDir(file.replace(src, dst), function (err) {
+                    if (err) {
+                        fs.appendFileSync('error.log', jsonDate + '\t\t' + err);
+                    }
+                });
+                walk(file);
             } else {
-                console.log('ERROR: ' + err);
-            }
-        });
-}, cnt);
+                // Handle files
+                var ext = path.extname(file);
 
-var walk = function (root, images) {
-    var files = fs.readdirSync(root);
-    for (var f in files) {
-        cnt++;
-        var file = path.join(root, files[f]);
-        var stat = fs.statSync(file);
-
-        var dir = path.dirname(file);
-        var filename = path.basename(file);
-        console.log(dir);
-        console.log(filename);
-
-        // Delete 'Thumbs.db', .DS_Store, .bridgelabelsandratings... and really anything that starts with a '.'
-        var basefile = file.split('\\');
-        if (basefile[basefile.length - 1] === 'Thumbs.db' || basefile[basefile.length - 1].charAt(0) === '.') {
-            fs.unlink(file, function(err){
-                if (err) throw err;
-            });
-        }
-
-        if (stat.isDirectory()) {
-            walk(file);
-        } else {
-            var ext = path.extname(file);
-
-            // ------ Gallery-Photos ------
-            if (file.indexOf('gallery-photos') > -1 && ext.toLowerCase() === '.jpg') {
-                if (file.indexOf('_sm') == -1) {
+                if (file.indexOf('gallery-photos') > -1 && ext.toLowerCase() === '.jpg') {
+                    // ------ Gallery-Photos ------
                     processQueue.push({ file: file, width: 360, suffix: '_sm' }, function (err) {
-                        if (!err) {
-                            //console.log('Processing: ', file);
-                        } else {
-                            console.log('ERROR: ', err);
+                        if (err) {
+                            fs.appendFileSync('error.log', jsonDate + '\t\t' + err);
                         }
                     });
-                }
-                processQueue.push({ file: file, width: 800, suffix: '' }, function (err) {
-                    if (!err) {
-                        console.log('Processing: ', file);
-                    } else {
-                        console.log('ERROR: ', err);
-                    }
-                });
-            }
 
-            // ------ Index-Photos ------
-            if (file.indexOf('index-photos') > -1 && ext.toLowerCase() === '.jpg') {
-                if (file.indexOf('_md') == -1) {
+                    processQueue.push({ file: file, width: 800, suffix: '' }, function (err) {
+                        if (err) {
+                            fs.appendFileSync('error.log', jsonDate + '\t\t' + err);
+                        }
+                    });
+                } else if (file.indexOf('index-photos') > -1 && ext.toLowerCase() === '.jpg') {
+                    // ------ Index-Photos ------
                     processQueue.push({ file: file, width: 760, suffix: '_md' }, function (err) {
-                        if (!err) {
-                            //console.log('Successfully Processed: ', file);
-                        } else {
-                            console.log('Error: ', err);
+                        if (err) {
+                            fs.appendFileSync('error.log', jsonDate + '\t\t' + err);
                         }
                     });
-                }
-                processQueue.push({ file: file, width: 2048, suffix: '' }, function (err) {
-                    if (!err) {
-                        //console.log('Successfully Processed: ', file);
-                    } else {
-                        console.log('Error: ', err);
-                    }
-                });
-            }
 
-            // ------ Other Images... ------
-            if (file.indexOf('index-photos') == -1 && file.indexOf('gallery-photos') == -1 && file.indexOf('floorplans') == -1 && file.indexOf('forms') == -1 && ext.toLowerCase() === '.jpg') {
-                if (file.indexOf('_sm') == -1) {
+                    processQueue.push({ file: file, width: 2048, suffix: '' }, function (err) {
+                        if (err) {
+                            fs.appendFileSync('error.log', jsonDate + '\t\t' + err);
+                        }
+                    });
+                } else if (file.indexOf('index-photos') == -1 && file.indexOf('gallery-photos') == -1 && file.indexOf('floorplans') == -1 && file.indexOf('forms') == -1 && ext.toLowerCase() === '.jpg') {
+                    // ------ Other (jpg) images ------
                     processQueue.push({ file: file, width: 800, suffix: '_sm' }, function (err) {
-                        if (!err) {
-                            //console.log('Successfully Processed: ', file);
-                        } else {
-                            console.log('Error: ', err);
+                        if (err) {
+                            fs.appendFileSync('error.log', jsonDate + '\t\t' + err);
                         }
                     });
-                }
-                processQueue.push({ file: file, width: 2048, suffix: '' }, function (err) {
-                    if (!err) {
-                        //console.log('Successfully Processed: ', file);
-                    } else {
-                        console.log('Error: ', err);
+
+                    processQueue.push({ file: file, width: 2048, suffix: '' }, function (err) {
+                        if (err) {
+                            fs.appendFileSync('error.log', jsonDate + '\t\t' + err);
+                        }
+                    });
+                } else {
+                    var basefile = file.split('\\');
+                    // Just copy over the other files...
+                    if (basefile[basefile.length -1] !== 'Thumbs.db' && basefile[basefile.length -1].charAt(0) !== '.'){
+                        fs.copy(file, file.replace(src, dst), function (err) {
+                            if (err) {
+                                fs.appendFileSync('error.log', err);
+                            }
+                        })
                     }
-                });
+                }
             }
-
-            // Rename directories
-            // ....
-            // ...
-            // ..
-            // .
-
         }
-        cnt++;
-        processQueue.concurrency = cnt;
+    } else {
+        console.log('\nIncorrect number of parameters. Please provide a source and destination.\n\tExample: node GraphicsMagick.js sourceImages destinationImages')
     }
 }
 
-walk('./properties');
+walk(src);
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ GraphicsMagick
 ==============
 
 Testing GraphicsMagick from node.
+
+This will copy images from specific directories in the source directory and convert/copy them to the destination directory.
+Other files will be copied from the source directory to the destination directory.
+
+Usage: node GraphicsMagick.js <source directory> <destination directory>
